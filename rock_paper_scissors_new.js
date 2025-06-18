8086cd67709b30de1216b58573b6ac3233d98c35
jich3n <jichen@ucsb.edu>
2025-06-18 16:14:21 +0800

    Generate a UI version of rock, paper, scissors

--- rock_paper_scissors_new.js
+++ rock_paper_scissors_new.js
@@ -1,3 +1,7 @@
+let result = document.querySelector("div");
+let final_result = document.querySelector(".result");
+let btns = document.querySelectorAll("button");
+
 // Extracting the computer's choice of rock, paper, or scissors
 function getComputerChoice(){
     let random_num = Math.floor(Math.random() * 3);
@@ -13,9 +17,9 @@ function getComputerChoice(){
 }
 
 // Extracting the player's choice of rock, paper, or scissors
-function getHumanChoice(){
-    return prompt("Rock, Paper, or Scissors");
-}
+// function getHumanChoice(){
+//     return prompt("Rock, Paper, or Scissors");
+// }
 
 // Initialize score variables
 let computer_score = 0;
@@ -25,36 +29,53 @@ let human_score = 0;
 function playRound(humanChoice, computerChoice) {
     humanChoice_cap = humanChoice[0].toUpperCase() + humanChoice.slice(1).toLowerCase()
     if (humanChoice_cap === computerChoice) {
-        alert("You tied.")
+        result.textContent = "You tied this round."
     } else if ((humanChoice_cap === "Rock" && computerChoice === "Scissors") || 
                (humanChoice_cap === "Paper" && computerChoice === "Rock") || 
                (humanChoice_cap === "Scissors" && computerChoice === "Paper")) {
-        alert("You win!");
         human_score ++;
+        result.textContent = "You win this round!";
     }
     else if ((humanChoice_cap === "Scissors" && computerChoice === "Rock") || 
              (humanChoice_cap === "Rock" && computerChoice === "Paper") || 
              (humanChoice_cap === "Paper" && computerChoice === "Scissors")){
-        alert("You lose.");
         computer_score ++;
+        result.textContent = "You lose this round.";
     }
     else {
-        alert("Invalid output. The round is wasted.");
+        result.textContent = "Invalid output. The round is wasted.";
     }
   }
   
-  const humanSelection = getHumanChoice();
-  const computerSelection = getComputerChoice();
+// const humanSelection = getHumanChoice();
+// let computerSelection = getComputerChoice();
   
-  playRound(humanSelection, computerSelection);
+// playRound(humanSelection, computerSelection);
 
-// Plays 5 rounds and alert the result of the game
-function playGame(){
-    for (let i=1; i<=5; i++) {
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
+btns.forEach(btn => {
+    btn.addEventListener("click", (e) => {
+        let target = e.target;
+        let humanSelection = '';
+        let computerSelection = '';
+        switch (target.id) {
+            case 'rock':
+                humanSelection = "rock";
+                break;
+            case 'paper':
+                humanSelection = "paper";
+                break;
+            case 'scissors':
+                humanSelection = "scissors";
+                break;
+        }
+
+        computerSelection = getComputerChoice();
         playRound(humanSelection, computerSelection);
-    }
-    alert(`Your score: ${human_score}\n Computer score: ${computer_score}`);
-}
-  
+
+        if (computer_score === 5) {
+            final_result.textContent = "The computer wins the entire game.";
+        } else if (human_score === 5) {
+            final_result.textContent = "Congratulations! You win the entire game.";
+        }
+    })
+});
